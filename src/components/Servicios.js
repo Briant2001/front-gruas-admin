896d3c10bc0a85f1a8006d98fee308d78a052181
Israel <118240268+IsraMictlan@users.noreply.github.com>
2023-05-30 18:31:50 -0600

    Add files via upload

---
+++ src/components/Servicios.js
@@ -0,0 +1,33 @@
+import "../assest/css/Servicios.css"
+export const Servicios = ()=>{
+
+    return(
+        <div className="back">
+            <table>
+            <thead>
+      <tr>
+        <th>Nombre</th>
+        <th>Correo</th>
+        <th>Entrega</th>
+        <th>Fecha</th>
+        <th>Fecha</th>
+        <th>Lugar</th>
+       
+        <th>Telefono</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+      </tr>
+    </tbody>
+            </table>
+        </div>
+    )
+ }
